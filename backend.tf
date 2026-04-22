a66795a2cb13347619a57b21b039c0556aff5f94
Adesegun Asunmo <adesegunabayomi@gmail.com>
2026-04-22 11:38:02 +0100

    EpicBook Infrastructure Pipeline (Stage 1)

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "epicbooktfstate01"
+    storage_account_name = "tfstate260420bc762b"
     container_name       = "tfstate"
     key                  = "epicbook-prod.tfstate"
   }
